dd092c438cd7132a830cd12985111fe4ca0c8788
Joyzo <jaknazeny@gmail.com>
2021-03-28 21:01:33 +0200

    093 Configuring & Using Themes

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
+      title: 'Personal Expenses',
+      theme: ThemeData(
+        primarySwatch: Colors.lime,
+        accentColor: Colors.amber,
+      ),
       home: MyHomePage(),
     );
   }
@@ -70,7 +74,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter App'),
+        title: Text('Personal Expenses'),
         actions: [
           IconButton(
               icon: Icon(Icons.add),
