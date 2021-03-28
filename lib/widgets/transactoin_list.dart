a7283c5c4482d2267bdc6690910b49fbdd717202
Joyzo <jaknazeny@gmail.com>
2021-03-28 12:51:16 +0200

    087 Making the List Scrollable!

--- lib/widgets/transactoin_list.dart
+++ lib/widgets/transactoin_list.dart
@@ -9,46 +9,51 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: transactions.map((tx) {
-      return Card(
-        child: Row(children: [
-          Container(
-            margin: EdgeInsets.symmetric(
-                vertical: 10,
-                horizontal: 15
-            ),
-            decoration: BoxDecoration(
-              border: Border.all(
-                  color: Colors.purple,
-                  width: 2),
-            ),
-            padding: EdgeInsets.all(10),
-            child: Text(
-              'A: \$${tx.amount}',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-                color: Colors.purple,
+    return Container(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(children: transactions.map((tx) {
+          return Card(
+            child: Row(children: [
+              Container(
+                margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 15
+                ),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                      color: Colors.purple,
+                      width: 2),
+                ),
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  'A: \$${tx.amount}',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.purple,
+                  ),
+                ),
               ),
-            ),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(tx.title,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                ),),
-              Text(
-                DateFormat('yyyy-MM-dd').format(tx.date),
-                style: TextStyle(
-                  color: Colors.grey,
-                ),),
-            ],)
-        ],),
-      );
-    }).toList(),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(tx.title,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),),
+                  Text(
+                    DateFormat('yyyy-MM-dd').format(tx.date),
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),),
+                ],)
+            ],),
+          );
+        }).toList(),
+        ),
+      ),
     );
   }
 }
